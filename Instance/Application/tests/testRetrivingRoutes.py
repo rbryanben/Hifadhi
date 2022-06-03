18b126963463adc3d90f0948ee5e18379698eb46
rbryanben <rbryanben@gmail.com>
2022-06-03 16:47:58 +0200

    Shard Retrival Tests: Incomplete

--- Instance/Application/tests/testRetrivingRoutes.py
+++ Instance/Application/tests/testRetrivingRoutes.py
@@ -3,11 +3,15 @@
     - Retriving private file (Stream & Download)
     - Retriving private file with signature
     - Retriving private file with ipv4 
+    - Retriving private file deleted signature 
+    - Retriving private file expired signature
+    - Retriving private file deleted ipv4 access
+    - Retriving private file expired ipv4 access
 """
 
 from urllib import response
 from django.test import TestCase
-import os
+import os, time
 
 class TestDownloadClass(TestCase):
 
@@ -93,7 +97,7 @@ class TestDownloadClass(TestCase):
         #test download       
         result = self.client.get(f"/api/v1/stream/{signature}")
         self.assertEqual(result.status_code,200)
-        
+
 
     """
         Retriving private file with ipv4 
@@ -119,8 +123,156 @@ class TestDownloadClass(TestCase):
         #test stream
         result = self.client.get(f"/api/v1/stream/{queryString}")
         self.assertEqual(result.status_code,200)
+
+    """
+        Retriving private file deleted signature 
+    """
+    def testRetrivingPrivateFileAfterSignatureDelete(self):
+
+        #upload private file 
+        self.testFile.seek(0,0)
+        result = self.client.post("/api/v1/store",{"file":self.testFile,"filename":"rex_private.jpg","mode":"private","override":"true"},
+            HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
+
+        self.assertEqual(result.status_code,200)
+
+
+        #get presigned URL 
+        result = self.client.get(f"/api/v1/presign/{os.environ.get('INSTANCE_NAME')}@rex_private.jpg",{"duration":2}
+            ,HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
+        presignedURL_ = result.content.decode('utf8')
+
+        #test download (200)    
+        result = self.client.get(f"/api/v1/download/{presignedURL_}")
+        self.assertEqual(result.status_code,200)
+
+        #test stream  (200)
+        result = self.client.get(f"/api/v1/stream/{presignedURL_}")
+        self.assertEqual(result.status_code,200)
+
+
+        #delete the signature
+        tokens = presignedURL_.split("=")
+
+        #delete
+        result = self.client.delete(f"http://localhost:8000/api/v1/presign?signature={tokens[1]}",HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
+        self.assertEqual(result.status_code,200)
+
+        #test download (200)    
+        result = self.client.get(f"/api/v1/download/{presignedURL_}")
+        self.assertEqual(result.status_code,401)
+
+        #test stream  (200)
+        result = self.client.get(f"/api/v1/stream/{presignedURL_}")
+        self.assertEqual(result.status_code,401)
         
+    """
+       Retriving private file expired signature 
+    """
+    def testRetrivalSignatureExpired(self):
+
+        #upload private file 
+        self.testFile.seek(0,0)
+        result = self.client.post("/api/v1/store",{"file":self.testFile,"filename":"rex_private.jpg","mode":"private","override":"true"},
+            HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
+
+        self.assertEqual(result.status_code,200)
 
 
+        #get presigned URL 
+        result = self.client.get(f"/api/v1/presign/{os.environ.get('INSTANCE_NAME')}@rex_private.jpg",{"duration":1}
+            ,HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
+        presignedURL_ = result.content.decode('utf8')
+
+        #test download (200)    
+        result = self.client.get(f"/api/v1/download/{presignedURL_}")
+        self.assertEqual(result.status_code,200)
+
+        #test stream  (200)
+        result = self.client.get(f"/api/v1/stream/{presignedURL_}")
+        self.assertEqual(result.status_code,200)
+
+        #sleep for 2 seconds
+        time.sleep(1)
+
+        #test download (200)    
+        result = self.client.get(f"/api/v1/download/{presignedURL_}")
+        self.assertEqual(result.status_code,401)
+
+        #test stream  (200)
+        result = self.client.get(f"/api/v1/stream/{presignedURL_}")
+        self.assertEqual(result.status_code,401)
+
+
+    """
+        Retriving private file deleted ipv4 access
+    """
+    def testRetrivingDeletedIPv4Access(self):
+
+        #upload private file 
+        self.testFile.seek(0,0)
+        result = self.client.post("/api/v1/store",{"file":self.testFile,"filename":"rex_private.jpg","mode":"private","override":"true"},
+            HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
+
+        self.assertEqual(result.status_code,200)
+        queryString = result.content.decode("utf8") 
+
+        #get IPv4 access 
+        result = self.client.get(f"/api/v1/ipv4access/{os.environ.get('INSTANCE_NAME')}@rex_private.jpg",{"duration":1
+            ,"ipv4":"127.0.0.1"},HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
+        
+        #test download       
+        result = self.client.get(f"/api/v1/download/{queryString}")
+        self.assertEqual(result.status_code,200)
+
+        #test stream
+        result = self.client.get(f"/api/v1/stream/{queryString}")
+        self.assertEqual(result.status_code,200)
+
+        #delete the ipv4 Access 
+        result = self.client.delete(f"/api/v1/ipv4access/{os.environ.get('INSTANCE_NAME')}@rex_private.jpg",{"duration":1
+            ,"ipv4":"127.0.0.1"},HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
 
     
+       
+        #test download       
+        result = self.client.get(f"/api/v1/download/{queryString}")
+        self.assertEqual(result.status_code,200)
+
+        #test stream
+        result = self.client.get(f"/api/v1/stream/{queryString}")
+        self.assertEqual(result.status_code,200)
+
+    """
+        Retriving private file expired ipv4 access
+    """
+    def testRetrivalExpiredIPv4(self):
+        #upload private file 
+        self.testFile.seek(0,0)
+        result = self.client.post("/api/v1/store",{"file":self.testFile,"filename":"rex_private.jpg","mode":"private","override":"true"},
+            HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
+
+        self.assertEqual(result.status_code,200)
+        queryString = result.content.decode("utf8") 
+
+        #get IPv4 access 
+        result = self.client.get(f"/api/v1/ipv4access/{os.environ.get('INSTANCE_NAME')}@rex_private.jpg",{"duration":1
+            ,"ipv4":"127.0.0.1"},HTTP_SHARD_KEY=os.environ.get("SHARD_KEY"))
+        
+        #test download       
+        result = self.client.get(f"/api/v1/download/{queryString}")
+        self.assertEqual(result.status_code,200)
+
+        #test stream
+        result = self.client.get(f"/api/v1/stream/{queryString}")
+        self.assertEqual(result.status_code,200)
+
+        time.sleep(1)
+           
+        #test download       
+        result = self.client.get(f"/api/v1/download/{queryString}")
+        self.assertEqual(result.status_code,401)
+
+        #test stream
+        result = self.client.get(f"/api/v1/stream/{queryString}")
+        self.assertEqual(result.status_code,401)
--- Instance/Application/tests/testShardRetrival.py
+++ Instance/Application/tests/testShardRetrival.py
@@ -1,14 +1,118 @@
+"""
+    - Retriving file when instance is not in a shard 
+    - Retring file from an instance that does not exist
+    - Retriving file that does not exist from another instance
+    - Retriving public file from a shard 
+    - Retriving public file from a shard
+    - Retriving private file from a shard without signature or ipv4 access  
+    - Retriving private file from a shard with signature
+    - Retriving private file from a shard with ipv4Access 
+    - Retriving private file from a shard with deleted signature
+    - Retriving private file from a shard with expired signature
+    - Retriving private file from a shard with deleted ipv4Access
+    - Retriving private file from a shard with expired ipv4Access 
+    - Retrive file after it has been updated on the other instance
+"""
 from django.test import TestCase
-import os
+import os, json, requests
+from requests_toolbelt.multipart import encoder
+
+#decorator for shard registreation 
+def shardRequired(func):
+    if "GOSSIP_INSTANCE" not in os.environ: return
+    
+    def inner(*args,**kwargs):
+        func(*args,**kwargs)
+    
+    return inner
+
+def  shardNotRequired(func):
+    if "GOSSIP_INSTANCE" in os.environ: return
+    def inner(*args,**kwargs):
+        func(*args,**kwargs)
+
+    return inner
+
 
 class testShardRetrival(TestCase):
+
+    @shardRequired
     def setUp(self):
         self.instance_name = os.environ.get("INSTANCE_NAME")
-    
-    def testInstanceIpv4Retrival(self):
-        #check if the gossip instance is defined
-        if "GOSSIP_INSTANCE" not in os.environ: return
+        self.gossip_instance_ip = os.environ.get("GOSSIP_INSTANCE")
+        
+        #get the instance name
+        registeredInstances = requests.get(f'http://{self.gossip_instance_ip}/api/v1/registered_instances',
+            headers={"SHARD-KEY":os.environ.get("SHARD_KEY")})
 
-        #retrieve the IPv4
-        response = self.client.post(f'http://{os.environ.get("GOSSIP_INSTANCE")}/api/v1/shard_instance',{"instance_name":self.instance_name},HTTP_SHARD_KEY="2022RBRYANBEN")
-        self.assertEqual(response.status_code,200)
+  
+        #load all instances to a dictionary
+        self.instances = json.loads(registeredInstances.text)
+
+        for instance in self.instances:
+            if self.instances[instance]["ipv4"] == self.gossip_instance_ip:
+                self.test_instance = instance 
+                break
+        
+        #test file
+        self.testFile = open("./Storage/Tests/dog.jpg","rb")
+
+        #upload a public file 
+        self.testFile.seek(0,0)
+        
+
+        form = encoder.MultipartEncoder({
+            "file": ("file",self.testFile, "application/octet-stream"),
+            "filename":"rex_public.jpg",
+            "override":"true"
+        })
+
+        result = requests.post(f"http://{self.gossip_instance_ip}/api/v1/store",data=form,
+            headers={"SHARD-KEY":os.environ.get("SHARD_KEY"),"Prefer": "respond-async", "Content-Type": form.content_type})
+
+        #upload a private file
+        self.testFile.seek(0,0)
+        
+
+        form = encoder.MultipartEncoder({
+            "file": ("file",self.testFile, "application/octet-stream"),
+            "filename":"rex_private.jpg",
+            "override":"true",
+            "mode": "private"
+        })
+
+        result = requests.post(f"http://{self.gossip_instance_ip}/api/v1/store",data=form,
+            headers={"SHARD-KEY":os.environ.get("SHARD_KEY"),"Prefer": "respond-async", "Content-Type": form.content_type})
+
+
+       
+    
+    """
+        Retriving file when instance is not in a shard 
+    """
+    @shardNotRequired
+    def testRetrivingWhenNotInShard(self):
+        result = self.client.get('/api/v1/download/KALAHARIANONYMOUS@demo.mp4')
+        self.assertEqual(500,result.status_code)
+
+    """
+        Retriving file from an instance that does not exist (With and Without Key)
+    """
+    @shardRequired
+    def testShardRetrivalInvalidInstance(self):
+        result = self.client.get('/api/v1/download/KALAHARIANONYMOUS@demo.mp4')
+        self.assertEqual(result.status_code,404)
+        
+    """
+        Retriving file that does not exist from another instance
+    """
+    def testRetrivingNonExistingFileFromShard(self):
+        result = self.client.get(f'/api/v1/download/{self.test_instance}@shouldNotExist.mp4')
+        self.assertEqual(result.status_code,404)
+
+    """
+        Retriving public file from a shard
+    """
+    def testRetrivingPublicFileFromShard(self):
+        pass
+    
